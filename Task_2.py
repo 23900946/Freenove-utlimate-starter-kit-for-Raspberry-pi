09123b9859309cd157f4c77ed57d484667711bce
Tommy Maher <76079816+23900946@users.noreply.github.com>
2022-09-30 17:02:18 +0100

    Add files via upload

---
+++ Task_2.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import time
+import RPi.GPIO as GPIO
+
+LEDRed = 11
+Button = 12
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(LEDRed, GPIO.OUT)
+GPIO.setup(Button, GPIO.IN)
+
+while True:
+    if GPIO.input(Button) == False:
+        print("Button pressed")
+        GPIO.output(LEDRed, GPIO.HIGH)
+        
+    else:
+        GPIO.output(LEDRed, GPIO.LOW)
+        os.system('clear')
+        print("Waitng for button press")
+        
+    time.sleep(2)
+
