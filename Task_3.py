3395eacf956ad4168cb466a19159333bdccbf44f
Tommy Maher <76079816+23900946@users.noreply.github.com>
2022-10-01 12:23:33 +0100

    Add files via upload

---
+++ Task_3.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import time
+import RPi.GPIO as GPIO
+
+
+LEDpins = [11, 12, 13, 15, 16, 18, 22, 3, 5, 24]
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(LEDpins, GPIO.OUT)
+
+while True:
+    for pins in LEDpins:
+        GPIO.output(pins, GPIO.LOW)
+        time.sleep(1)
+        GPIO.output(pins, GPIO.HIGH)
+        
+    for pins in LEDpins[::-1]:
+        GPIO.output(pins, GPIO.LOW)
+        time.sleep(1)
+        GPIO.output(pins, GPIO.HIGH)
