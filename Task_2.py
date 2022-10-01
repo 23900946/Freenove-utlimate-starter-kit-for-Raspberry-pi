850883632757bd9721b9ec8b85ce1c1948bd2fbb
Tommy Maher <76079816+23900946@users.noreply.github.com>
2022-10-01 12:26:09 +0100

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -11,13 +11,13 @@ GPIO.setwarnings(False)
 GPIO.setup(LEDRed, GPIO.OUT)
 GPIO.setup(Button, GPIO.IN)
 
-while True:
-    if GPIO.input(Button) == False:
+while True: 
+    if GPIO.input(Button) == False: # LED will switch on when button is pressed
         print("Button pressed")
         GPIO.output(LEDRed, GPIO.HIGH)
         
     else:
-        GPIO.output(LEDRed, GPIO.LOW)
+        GPIO.output(LEDRed, GPIO.LOW) # LED will be in a off state until button is pressed
         os.system('clear')
         print("Waitng for button press")
         
